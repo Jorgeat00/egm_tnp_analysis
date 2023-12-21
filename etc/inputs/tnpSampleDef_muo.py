a037e6e8fc999cbab5b891fd9dac60a832d6af8b
Barbara Alvarez <balvarez@cern.ch>
2023-12-21 18:20:30 +0100

    etc/inputs/tnpSampleDef_ele.py

--- etc/inputs/tnpSampleDef_muo.py
+++ etc/inputs/tnpSampleDef_muo.py
@@ -1,10 +1,16 @@
 from libPython.tnpClassUtils import tnpSample
 
-### qll stat
-dirtest = '/pool/phedex/userstorage/balvarez/TnP_ttbarRun3_merged/'
-diralt = '/pool/phedex/userstorage/balvarez/TnP_ttbarRun3_alt_merged/'
-test = {'DY'   : tnpSample('DY',   dirtest + 'Tree_DYJetsToLL_M50_Muon_0.root', isMC = True, nEvts = -1), 
-        'DYAlt': tnpSample('DYAlt', diralt + 'Tree_DYJetsToLL_M50_Muon_0.root', isMC = True, nEvts = -1), 
-        'data' : tnpSample('data', dirtest + 'Tree_*Muon_Run2022_0.root', lumi = 1.2),
+#dirMC = '/beegfs/data/nanoAODv11/TnP/preliminaryMuonMC/'
+#dirMCalt = '/beegfs/data/nanoAODv11/TnP/preliminaryMuonMCAlt/'
+#dirdata = '/beegfs/data/nanoAODv11/TnP/preliminaryMuonDATA/'
+#dirMC = '/beegfs/data/nanoAODv11/TnP/Muon/MC/'
+#dirMCalt = '/beegfs/data/nanoAODv11/TnP/Muon/MCAlt/'
+#dirdata = '/beegfs/data/nanoAODv11/TnP/Muon/DATA/'
+dirMC = '/lustrefs/hdd_pool_dir/nanoAODv11/TnP/Muon_conept_v2/MC/'
+dirMCalt = '/lustrefs/hdd_pool_dir/nanoAODv11/TnP/Muon_conept_v2/MCAlt/'
+dirdata = '/lustrefs/hdd_pool_dir/nanoAODv11/TnP/Muon_conept_v2/Data/'
+test = {'DY'   : tnpSample('DY',   dirMC + 'DYJetsToLL_M-50_part*.root', isMC = True, nEvts = -1), 
+        'DYAlt': tnpSample('DYAlt', dirMCalt + 'DYJetsToLL_M-50_part*.root', isMC = True, nEvts = -1), 
+        'data' : tnpSample('data', dirdata + '*Run2022*.root', lumi = 20.7),
 }
 
