e2544402a64a14b9342ee371ab61c181d8cd9194
Barbara Alvarez <balvarez@cern.ch>
2023-12-21 18:51:07 +0100

    electron samples

--- etc/inputs/tnpSampleDef_ele.py
+++ etc/inputs/tnpSampleDef_ele.py
@@ -1,10 +1,13 @@
 from libPython.tnpClassUtils import tnpSample
 
-### qll stat
-dirtest = '/pool/phedex/userstorage/balvarez/TnP_ttbarRun3_merged/'
-diralt = '/pool/phedex/userstorage/balvarez/TnP_ttbarRun3_alt_merged/'
-test = {'DY'   : tnpSample('DY',   dirtest + 'Tree_DYJetsToLL_M50_Electron_0.root', isMC = True, nEvts = -1), 
-        'DYAlt': tnpSample('DYAlt', diralt + 'Tree_DYJetsToLL_M50_Electron_0.root', isMC = True, nEvts = -1), 
-        'data' : tnpSample('data', dirtest + 'Tree_EGamma_Run2022_0.root', lumi = 1.2),
+#dirMC = '/beegfs/data/nanoAODv11/TnP/preliminaryElectronMC/'
+#dirMCalt = '/beegfs/data/nanoAODv11/TnP/preliminaryElectronMCAlt/'
+#dirdata = '/beegfs/data/nanoAODv11/TnP/preliminaryElectronDATA/'
+dirMC = '/lustrefs/hdd_pool_dir/nanoAODv11/TnP/Electron_conept_v2/MC/'
+dirMCalt = '/lustrefs/hdd_pool_dir/nanoAODv11/TnP/Electron_conept_v2/MCAlt/'
+dirdata = '/lustrefs/hdd_pool_dir/nanoAODv11/TnP/Electron_conept_v2/Data/'
+test = {'DY'   : tnpSample('DY',   dirMC + 'DYJetsToLL_M-50_part*.root', isMC = True, nEvts = -1), 
+        'DYAlt': tnpSample('DYAlt', dirMCalt + 'DYJetsToLL_M-50_part*.root', isMC = True, nEvts = -1), 
+        'data' : tnpSample('data', dirdata + '*Run2022*.root', lumi = 20.7),
 }
 
